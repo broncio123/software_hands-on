3cc836d684fe259f13aac3decfb920d48c54ab65
Broncio Aguilar Sanjuan <sanjuan@hwamei.stats.ox.ac.uk>
2021-10-28 17:43:43 +0100

    added flask files and moved ablooper files

---
+++ webdevel/myflask.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/') # decorator
+def home():
+    '''viewing function - home page'''
+    return "Welcome home! :)"; 
+
+
+@app.route('/educative')
+def leanr():
+    '''viewing function - appended page'''
+    return "Ready to learn"
+
+if __name__ == "__main__":
+    app.run(debug = True, host = "0.0.0.0", port = 3000)
